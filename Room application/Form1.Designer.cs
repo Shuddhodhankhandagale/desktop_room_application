1d12b7a230a6917c62765a3e8e72fe0b7660c8ee
Annu the great <Annu the great@ANNU-THEGREAT>
2023-06-07 17:39:43 +0530

    Add project files.

---
+++ Room application/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Room_application
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txtfood = new System.Windows.Forms.TextBox();
+            this.txtin = new System.Windows.Forms.TextBox();
+            this.txtout = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtbill = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Customer_name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(389, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Customer_food";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Customer_indate";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(389, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(119, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Customer_outdate ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(62, 149);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "total_bill";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(142, 18);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(100, 22);
+            this.txtname.TabIndex = 5;
+            this.txtname.Text = "nad";
+            this.txtname.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtfood
+            // 
+            this.txtfood.Location = new System.Drawing.Point(508, 15);
+            this.txtfood.Name = "txtfood";
+            this.txtfood.Size = new System.Drawing.Size(100, 22);
+            this.txtfood.TabIndex = 6;
+            this.txtfood.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // txtin
+            // 
+            this.txtin.Location = new System.Drawing.Point(142, 69);
+            this.txtin.Name = "txtin";
+            this.txtin.Size = new System.Drawing.Size(100, 22);
+            this.txtin.TabIndex = 7;
+            this.txtin.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // txtout
+            // 
+            this.txtout.Location = new System.Drawing.Point(508, 75);
+            this.txtout.Name = "txtout";
+            this.txtout.Size = new System.Drawing.Size(100, 22);
+            this.txtout.TabIndex = 8;
+            this.txtout.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(274, 131);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 58);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtbill
+            // 
+            this.txtbill.Location = new System.Drawing.Point(142, 149);
+            this.txtbill.Name = "txtbill";
+            this.txtbill.Size = new System.Drawing.Size(100, 22);
+            this.txtbill.TabIndex = 11;
+            this.txtbill.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(392, 131);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 61);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Update ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(16, 195);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(772, 244);
+            this.dataGridView1.TabIndex = 13;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtbill);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtout);
+            this.Controls.Add(this.txtin);
+            this.Controls.Add(this.txtfood);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.TextBox txtfood;
+        private System.Windows.Forms.TextBox txtin;
+        private System.Windows.Forms.TextBox txtout;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtbill;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Room application/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Room_application
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con=new SqlConnection("Data Source=ANNU-THEGREAT;Initial Catalog=hotels;Integrated Security=True");
+        SqlCommand cmd=new SqlCommand();
+        int i = 0;
+        private object dataGridtxView1;
+        private SqlDataAdapter adapt;
+
+        public Form1()
+        {
+            InitializeComponent();
+            DisplayData();
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void DisplayData()
+        {
+            con.Open();
+            DataTable dt = new DataTable();
+            adapt = new SqlDataAdapter("select * from rooms", con);
+            adapt.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+        }
+
+        private void clearData()
+        {
+          /*//throw new NotImplementedException();*/
+          txtname.Text = string.Empty;
+            txtfood.Text = string.Empty;
+            txtin.Text = string.Empty;
+            txtout.Text = string.Empty; 
+            txtbill.Text = string.Empty; 
+            
+     
+
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtname.Text != "" && txtfood.Text != "" && txtin.Text != "" && txtout.Text != "" && txtbill.Text != "")
+            {
+
+                cmd=new SqlCommand("insert into rooms( Customer_name ,Customer_food, Customer_indate, Customer_outdate,total_bill) values (@Customer_name ,@Customer_food, @Customer_indate, @Customer_outdate,@total_bill)", con);
+                cmd.Parameters.AddWithValue("@Customer_name", txtname.Text);
+                cmd.Parameters.AddWithValue("@Customer_food", txtfood.Text);
+                cmd.Parameters.AddWithValue("@Customer_indate", txtin.Text);
+                cmd.Parameters.AddWithValue("@Customer_outdate", txtout.Text);
+                cmd.Parameters.AddWithValue("@total_bill", txtbill.Text);
+                con.Open();
+                 
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Record Inserted Successfully");
+               DisplayData();
+                clearData();
+
+
+               DisplayData ();  
+            }
+            else
+            {
+                MessageBox.Show(" Insert correct value");
+            }
+        }
+        
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            {
+                i = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString());
+                txtname.Text= dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+                txtfood.Text= dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+                txtin.Text= dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+                txtout.Text= dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+                txtbill.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+                
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+
+            {
+                try
+                {
+                    if (txtname.Text != "" && txtfood.Text != "" && txtin.Text != "" && txtout.Text != "" && txtbill.Text != "")
+                    {
+                        cmd = new SqlCommand("update rooms set Customer_name=@Customer_name,Customer_food=@Customer_food,Customer_indate=@Customer_indate,Customer_outdate=@Customer_outdate,total_bill=@total_bill where room_id=@room_id\r\n", con);
+                        con.Open();
+                        cmd.Parameters.AddWithValue("@Customer_name", txtname.Text);
+                        cmd.Parameters.AddWithValue("@Customer_fooo", txtfood.Text);
+                        cmd.Parameters.AddWithValue("@Customer_indate", txtin.Text);
+                        cmd.Parameters.AddWithValue("@Customer_outdate", txtout.Text);
+                        cmd.Parameters.AddWithValue("@total_bill", txtbill.Text);
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("Record Updated Successfully");
+                        con.Close();
+                        DisplayData();
+                        //ClearData();
+
+                        
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please Select Record to Update");
+                    }
+                }
+                catch (Exception ex)
+                {
+              
+                }
+            }
+
+        }        
+    }
+}
